6453f44bd74a3795e8a8e68cbb7a0f5e5940a55d
Toby Bessant <toby.bessant.1998@gmail.com>
2018-02-14 11:45:09 +0000

    Fixed 360 images
    - Reversed the normals on the spheres, 2 360 image scenes now functional (Witcher and one street scene).
    - Edited main menu button labels

---
+++ Assets/TobyVR/Scripts/DisplayContextUI.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayContextUI : MonoBehaviour
+{
+
+    private Camera cameraObj;
+    private Transform transform;
+
+    [SerializeField] private GameObject menuButton;
+
+    private float displayAngle = 50;
+
+    private bool displayed = false;
+
+    private void Start()
+    {
+        cameraObj = Camera.main;
+        transform = GetComponent<Transform>();
+        menuButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!displayed) //if UI is not currently displayed...
+        {
+            
+
+            if (CheckAngle() > displayAngle && CheckAngle() < 100) //evaluate the cameras current X value to be within a vertical area...
+            {
+                DisplayUI();
+                displayed = true;
+            }
+            
+        }
+        if (displayed) //if UI is currently displayed 
+        {
+            if (CheckAngle() < displayAngle) //evaluate if the cameras current X value is outside the area
+            {
+                HideUI();
+                displayed = false;
+            }
+            
+        }
+        
+
+    }
+
+    private float CheckAngle()
+    {
+        float xVal;
+
+        xVal = cameraObj.transform.eulerAngles.x;
+        
+        return xVal;
+    }
+
+    void DisplayUI()
+    {
+        transform.rotation = Quaternion.Euler(0, cameraObj.transform.rotation.eulerAngles.y, 0);
+        
+        menuButton.SetActive(true);
+
+    }
+    void HideUI()
+    {
+        menuButton.SetActive(false);
+
+    }
+}
