989728c3893a24693c3f8fccd30b8f03af4249d6
Toby Bessant <toby.bessant.1998@gmail.com>
2018-02-15 16:48:18 +0000

    main menu waypoints
    - Fixed voxel scene material bug
    - Switched main menu buttons to the waypoints themselves. Will make transition to new main menu easier.

---
+++ Assets/TobyVR/Scripts/InteractiveEnableMoreUI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveEnableMoreUI : MonoBehaviour, IInteractiveObject
+{
+    [SerializeField] private float gazeTimer = 1.0f; //Seconds taken to activate interactive event
+    [SerializeField] private GameObject[] ui = new GameObject[0];
+
+    private bool gazingAt = false; //flag 
+
+    public void Action()
+    {
+        EnableElements();
+        gameObject.SetActive(false);
+    }
+
+    public void GazeEnter()
+    {
+        gazingAt = true;
+    }
+
+    public void GazeExit()
+    {
+        gazeTimer = 1.0f;
+        gazingAt = false;
+    }
+
+    public void GazeTimer()
+    {
+        if (gazingAt)
+        {
+            gazeTimer -= Time.deltaTime;
+
+            Action();
+        }
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        for (int i = 0; i < ui.Length; i++)
+        {
+            ui[i].SetActive(false);
+        }
+    }
+
+    void EnableElements()
+    {
+        for (int i = 0; i < ui.Length; i++)
+        {
+            ui[i].SetActive(true);
+        }
+    }
+}
