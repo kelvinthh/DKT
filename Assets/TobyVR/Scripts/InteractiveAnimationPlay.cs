e80cd44942e71729b7c856d6f4664b990eba28f2
Toby Bessant <toby.bessant.1998@gmail.com>
2018-02-21 13:09:42 +0000

    Button animation
    - Started working on implementing button highlight animation

---
+++ Assets/TobyVR/Scripts/InteractiveAnimationPlay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveAnimationPlay : MonoBehaviour, IInteractiveObject {
+
+    [SerializeField] private float gazeTimer = 1.0f;
+
+    Animator anim;
+
+    private bool gazingAt = false;
+    public void Action()
+    {
+
+    }
+
+    public void GazeEnter()
+    {
+        anim.Play("button enlarge");
+    }
+
+    public void GazeExit()
+    {
+        anim.Play("Entry");
+    }
+
+    public void GazeTimer()
+    {
+    }
+
+    // Use this for initialization
+    void Awake () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
