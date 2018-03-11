e5192acb58a83066575eb7b722460eb2d75a5c8d
weegeekelvin <weegeekelvin@gmail.com>
2018-03-11 15:43:22 +0000

    Added 2 Buttons
    - Added a script for buttons
    - Added button to Play or Pause the clip
    - Added button to return to the previous scene

---
+++ Assets/Kelvin360Video/Scripts/VideoController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour {
+
+    public VideoPlayer videoPlayer;
+    public int sceneNumber;
+
+	// Use this for initialization
+
+    public void playAndPause()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            videoPlayer.Pause();
+        }else
+        {
+            videoPlayer.Play();
+        }
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
