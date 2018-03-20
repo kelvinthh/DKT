d185038701639b1a154d5288667c68b7571edd36
Fletcher Morris <odmoandtheboy@gmail.com>
2018-03-20 01:48:49 +0000

    Started work on wave system
    Created GameManager script.
    Created Level, Wave, and Difficulty classes.

---
+++ Assets/Space Game/Scripts/sg_GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sg_GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private int m_currentWave;
+    [SerializeField]
+    private int m_remainingEnemies;
+
+    [SerializeField]
+    private GameObject m_playerShip;
+
+    public int GetCurrentWave()
+    {
+        return m_currentWave;
+    }
+    public int GetRemainingEnemies()
+    {
+        return m_remainingEnemies;
+    }
+
+
+    private void Start()
+    {
+        m_playerShip = GameObject.FindGameObjectWithTag("Player");
+    }
+}
---
+++ Assets/Space Game/Scripts/sg_Level.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sg_Level
+{
+    public sg_LevelDifficulty difficulty;
+    public List<sg_Wave> waves;
+}
+
+public class sg_Wave
+{
+
+}
+
+public enum sg_LevelDifficulty
+{
+    Easy,
+    Medium,
+    Hard,
+    Boss
+}
