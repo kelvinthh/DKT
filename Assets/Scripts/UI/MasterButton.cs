7b577b40b9fad665898a351e83414942b4dab22f
Toby Bessant <toby.bessant.1998@gmail.com>
2018-04-22 18:03:01 +0100

    further changes to buttonscripts
    - started work on a 'master' button script that will contain all settings and properties for any given button.

---
+++ Assets/Scripts/UI/MasterButton.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.UI;
+using UnityEngine.SceneManagement;
+public enum ButtonType { LOADSCENE, TELEPORT };
+
+public class MasterButton : MonoBehaviour, IInteractiveObject {
+
+    [SerializeField] public ButtonType buttonAction;
+    [SerializeField] private float gazeTimer = 2.0f;
+    [SerializeField] public bool hasProgressBar;
+
+    [SerializeField] public Image progressBar;
+    [SerializeField] public GameObject teleportTo;
+
+    private float barProgress = 0.0f;
+    private float barSpeed = 50.0f;
+
+    [SerializeField] Transform player;
+
+    private bool gazingAt = false;
+
+    public void Action()
+    {
+        switch(buttonAction)
+        {
+            case ButtonType.LOADSCENE:
+                SceneManager.LoadScene(1);
+                break;
+
+            case ButtonType.TELEPORT:
+                
+                break;
+        }
+    }
+
+    public void GazeEnter()
+    {
+        gazingAt = true;
+    }
+
+    public void GazeExit()
+    {
+        gazingAt = false;
+    }
+
+    public void GazeTimer()
+    {
+        if (gazingAt)
+        {
+            gazeTimer -= Time.deltaTime;
+
+            UpdateBarProgress();
+
+            if (gazeTimer <= 0)
+            {
+                Action();
+            }
+        }
+    }
+
+    public void UpdateBarProgress()
+    {
+        if (barProgress < 100)
+        {
+             barProgress += barSpeed * Time.deltaTime;
+             print(barProgress);
+        }
+
+        if (progressBar != null) { progressBar.fillAmount = barProgress / 100; }
+       
+    }
+
+    private float GetProgressBarDuration()
+    {
+        return 100 / gazeTimer;
+    }
+
+    // Use this for initialization
+    void Start () {
+        GetProgressBarDuration();
+        progressBar = GetComponent<Image>();
+        player = GameObject.Find("PlayerWithRadialProg").GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        GazeTimer();
+        
+	}
+}
