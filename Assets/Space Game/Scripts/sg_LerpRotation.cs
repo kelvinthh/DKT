4030101ed5d0aa5752c58d557f808ed056054fb7
Fletcher Morris <odmoandtheboy@gmail.com>
2018-05-07 02:08:40 +0100

    HUD now follows view
    Created 'LerpRotation' script, which allows one objects rotation to lerp towards that of another.
    
    Added LerpRotation script to HUD Canvas.

---
+++ Assets/Space Game/Scripts/sg_LerpRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sg_LerpRotation : MonoBehaviour {
+
+    public Transform followTransform;
+    public float lerpSpeed = 10.0f;
+    public bool doFollow = true;
+    private Transform m_transform;
+
+    private void Start()
+    {
+        m_transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if(followTransform && m_transform && doFollow)
+        {
+            Quaternion a = m_transform.rotation;
+            Quaternion b = followTransform.rotation;
+            m_transform.rotation = Quaternion.Lerp(a, b, lerpSpeed * Time.deltaTime);
+        }
+    }
+}
