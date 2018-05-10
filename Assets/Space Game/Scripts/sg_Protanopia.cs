eff967dde121a9532790d7827096fb331ced9226
Fletcher Morris <odmoandtheboy@gmail.com>
2018-05-10 03:30:45 +0100

    Added colourblindness shader
    Created Protanopia shader, to estimate how things would look with the most common variety of colourblindness.
    
    Add 'sg_Protanopia' script to any camera to see the effect.

---
+++ Assets/Space Game/Scripts/sg_Protanopia.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class sg_Protanopia : MonoBehaviour {
+
+    private Shader protaniopiaShader;
+    private Material mat;
+    [Range(0, 1)]
+    public float blend = 1f;
+
+    private void Start()
+    {
+        if (protaniopiaShader == null) protaniopiaShader = Shader.Find("Space Game/Protanopia");
+        mat = new Material(protaniopiaShader);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (mat && protaniopiaShader)
+        {
+            mat.SetFloat("Blend", blend);
+            Graphics.Blit(source, destination, mat, 0);
+        }
+    }
+}
